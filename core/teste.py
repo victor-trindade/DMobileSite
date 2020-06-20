4ee2096aa2c09cf0cc37e821d74587e7c278c822
Victor <victor.trindade@outlook.com.br>
2020-06-20 18:10:47 -0300

    deletando arquivo teste

--- core/teste.py
+++
@@ -1,8 +0,0 @@
-#'/home/trindade/django/core'
-from pathlib import Path
-
-from core.server_settings import STATIC_ROOT, BASE_DIR1,BASE_DIR2
-
-
-print('Base dir os path',BASE_DIR1)
-print('Base dir pathlib',BASE_DIR2)
